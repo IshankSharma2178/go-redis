f87e388709295155bd291c7ae75dbc65252920c5
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2026-06-10 15:24:34 +0530

    AOF

--- core/resp.go
+++ core/resp.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 )
@@ -149,15 +150,28 @@ func DecodeArrayString(data []byte) ([]string, error) {
 	return tokens, nil
 }
 
+func encodeString(v string) []byte {
+	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+}
+
 func Encode(value interface{}, isSimple bool) []byte {
 	switch v := value.(type) {
 	case string:
 		if isSimple {
 			return []byte(fmt.Sprintf("+%s\r\n", v))
 		}
-		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+		return encodeString(v)
 	case int, int8, int16, int32, int64:
 		return []byte(fmt.Sprintf(":%d\r\n", v))
+	case []string:
+		var b []byte
+		buf := bytes.NewBuffer(b)
+		for _, b := range value.([]string) {
+			buf.Write(encodeString(b))
+		}
+		return []byte(fmt.Sprintf("*%d\r\n%s", len(v), buf.Bytes()))
+	case error:
+		return []byte(fmt.Sprintf("-%s\r\n", v))
 	default:
 		return RESP_NIL
 	}
--- internals/config/config.go
+++ internals/config/config.go
@@ -7,6 +7,7 @@ type Config struct {
 	Port             int    `yaml:"port"`
 	KeysLimit        int    `yaml:"keys_limit"`
 	EvictionStrategy string `yaml:"eviction_strategy"`
+	AOFFile          string `yaml:"aof_file"`
 }
 
 var Cfg = Config{
@@ -14,10 +15,12 @@ var Cfg = Config{
 	Port:             7379,
 	KeysLimit:        5,
 	EvictionStrategy: "simple-first",
+	AOFFile:          "./bin/appendonly.aof",
 }
 
 func SetupFlags() {
 	flag.StringVar(&Cfg.Host, "host", Cfg.Host, "host for the server")
 	flag.IntVar(&Cfg.Port, "port", Cfg.Port, "port for the server")
+	flag.StringVar(&Cfg.AOFFile, "aof-file", Cfg.AOFFile, "path to the AOF file")
 	flag.Parse()
 }
